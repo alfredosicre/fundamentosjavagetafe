5c9561067e382d59dd6ce2ced718d76900b9e4bc
alfredosicre <alfredo.sicre@gmail.com>
2025-06-04 13:50:16 +0200

    04junio202504

---
+++ Class08CalcularSalario.java
@@ -0,0 +1,16 @@
+// INTRODUZCA HORAS SEMANALES                                
+// INTRODUZCA IMPORTE HORA:        
+// INTRODUZCA KILOMETROS:  
+
+import java.util.Scanner;
+
+public class Class08CalcularSalario {
+
+    // psvm
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+
+    }
+    
+}
